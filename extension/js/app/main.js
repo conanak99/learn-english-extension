123f4db766ca289c4942fd47b7c4d1584f241757
Hoang Pham <huyhoang8a5@gmail.com>
2017-06-28 15:06:42 +0100

    Finish version 0.7

--- extension/js/app/main.js
+++ extension/js/app/main.js
@@ -1,108 +1,204 @@
 function getRandomElement(array) {
-    let randomIndex = Math.floor((Math.random() * array.length));
-    return array[randomIndex];
+  let randomIndex = Math.floor((Math.random() * array.length));
+  return array[randomIndex];
 }
-
-function getQuiz() {
-  const numberOfAnswer = 4;
-  const answers = [];
-  for (var i = 0; i < numberOfAnswer; i++) {
-    answers.push(getRandomElement(words));
-  }
-
-  const question = getRandomElement(answers);
-
-  return { question, answers };
-}
-
-async function fetchImageFromServer() {
-  var headers  = new Headers({
-    'x-api-key': 'qGugB8rM7728ZKbEH8Wxc6mDHic0ijq6iETMiZTh'
-  });
-
-  var k = await fetch(
-    'https://78tdi7b2n7.execute-api.ap-southeast-1.amazonaws.com/prod/GetRandomGirl',
-    { headers: headers });
-
-  var m = await k.json();
-  return m;
-}
-
-function getRewardImageInternal() {
-  // If local storage don't have many image
-  // fetchImageFromServer();
-
-  // Else read from local storage and return
-}
-
-function loadInitialImages() {
-  // Load inital image and put into local storage
-
-  // Remeber to check for duplicated
-
-  // Load score from storage sync
-}
-
-function saveScore(score) {
-
-}
-
-function getRewardImage(score) {
-  // Get image from local storage
-  // chrome.storage.local
-
-  return 'https://s-media-cache-ak0.pinimg.com/736x/72/0e/71/720e717befbd2127ecd66d2fc0efe6d8.jpg';
-}
-
-function preloadImage(imgUrl) {
-  let img = new Image();
-  img.src = imgUrl;
-}
-
 
 let module = angular.module("app", ['ngRoute']);
 
-module.controller('MainController', ['$scope', function($scope) {
-  $scope.score = 0;
-}]);
-
-module.controller('IndexController', ['$scope', '$location', function($scope, $location) {
-  $scope.quiz = getQuiz();
-
-  console.log($scope.quiz);
-
-  $scope.goToResult = (meaning) => {
-    let result = 0;
-    if (meaning === $scope.quiz.question.meaning) result = 1;
-    $location.path(`/result/${result}`);
+module.factory('StorageService', function() {
+  return {
+    getScore() {
+      return parseInt(localStorage.getItem('score') || 0);
+    },
+    setScore(score) {
+      localStorage.setItem('score', score);
+    },
+    getImageStore() {
+      var imageStore = localStorage.getItem('imageStore');
+      if (!imageStore) return { normal: [], sexy: []};
+      return JSON.parse(imageStore);
+    },
+    setImageStore(imageStore) {
+      localStorage.setItem('imageStore', JSON.stringify(imageStore));
+    }
   }
-}]);
+});
 
-module.controller('ResultController', ['$scope', '$routeParams', function($scope, $routeParams) {
-  $scope.result = $routeParams.result === '1';
+module.factory('QuizService', [
+  'StorageService',
+  function(StorageService) {
+    return {
+      score: StorageService.getScore(),
+      increaseScore: function(number) {
+        this.score += number;
+        StorageService.setScore(this.score);
+      },
+      reset: function() {
+        this.score = 0;
+        StorageService.setScore(this.score);
+      },
+      getQuiz: function() {
+        const numberOfAnswer = 4;
+        const answers = [];
 
-  if ($scope.result) {
-    // Use another module to get image
-    $scope.text = "Tiếp tục";
-    $scope.resultUrl = getRewardImage(0);
-    // Use another module to increase score
-  } else {
-    // Reset score to zero
-    $scope.text = "Bắt đầu lại";
-    let wrongImages = ['polla-1.jpg', 'polla-2.jpg', 'polla-3.jpg', 'polla-4.jpg', 'polla-5.jpg'];
-    $scope.resultUrl = 'img/wrong/' + getRandomElement(wrongImages);
+        let i = 0;
+        while (i < numberOfAnswer) {
+          let randomAnswer = getRandomElement(words);
+          if (answers.indexOf(randomAnswer) === -1 && randomAnswer.meaning.length > 0) {
+            answers.push(randomAnswer);
+            i++
+          }
+        }
+
+        const question = getRandomElement(answers);
+        return {question, answers};
+      }
+    }
   }
+]);
 
-}]);
+module.factory('ImageService', [
+  'StorageService',
+  function(StorageService) {
+    function preloadImage(imgUrl) {
+      let img = new Image();
+      img.src = imgUrl;
+    }
 
-module.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $routeProvider
-   .when('/', {
-    templateUrl: 'js/app/template/index.html',
-    controller: 'IndexController',
-  })
-  .when('/result/:result', {
-    templateUrl: 'js/app/template/result.html',
-    controller: 'ResultController'
-  });
-}]);
+    return {
+      imageStore: StorageService.getImageStore(),
+      fetchImageFromAPI: async() => {
+        var headers = new Headers({'x-api-key': 'qGugB8rM7728ZKbEH8Wxc6mDHic0ijq6iETMiZTh'});
+        var fetchResult = await fetch('https://78tdi7b2n7.execute-api.ap-southeast-1.amazonaws.com/prod/GetRandomGirl', {headers: headers});
+        return await fetchResult.json();
+      },
+
+      initializeStore: async function() {
+        if (this.imageStore.normal.length === 0) {
+          let imageStore = await this.fetchImageFromAPI();
+          StorageService.setImageStore(imageStore);
+          this.imageStore = imageStore;
+
+          imageStore.normal.forEach(preloadImage);
+          imageStore.sexy.forEach(preloadImage);
+        }
+      },
+
+      loadMoreImageIntoStore: async function() {
+        let newImages = await this.fetchImageFromAPI();
+
+        newImages.normal.forEach(imageUrl => {
+          if (this.imageStore.normal.indexOf(imageUrl) === -1) {
+            this.imageStore.normal.push(imageUrl);
+            preloadImage(imageUrl);
+          }
+        });
+        newImages.sexy.forEach(imageUrl => {
+          if (this.imageStore.sexy.indexOf(imageUrl) === -1) {
+            this.imageStore.sexy.push(imageUrl);
+            preloadImage(imageUrl);
+          }
+        });
+        StorageService.setImageStore(this.imageStore);
+      },
+
+      getRewardImage: function(score) {
+        var imageUrl = '';
+        if (score % 5 === 0) {
+          imageUrl = this.imageStore.sexy.shift();
+        } else {
+          imageUrl = this.imageStore.normal.shift();
+        }
+        StorageService.setImageStore(this.imageStore);
+
+        // Fetch more if no image found
+        if (this.imageStore.normal.length < 6) {
+          this.loadMoreImageIntoStore();
+        }
+        return imageUrl;
+      },
+      getPunishImage: () => {
+        let wrongImages = ['polla-1.jpg', 'polla-2.jpg', 'polla-3.jpg', 'polla-4.jpg', 'polla-5.jpg'];
+        return '/img/wrong/' + getRandomElement(wrongImages);
+      }
+    };
+  }
+]);
+
+module.controller('MainController', [
+  '$scope',
+  'QuizService',
+  'ImageService',
+  function($scope, QuizService, ImageService) {
+    $scope.quizService = QuizService;
+
+    $scope.isLoading = true;
+    ImageService.initializeStore().then(() => {
+      $scope.isLoading = false;
+      $scope.$digest();
+    })
+  }
+]);
+
+module.controller('IndexController', [
+  '$scope',
+  '$location',
+  'QuizService',
+  function($scope, $location, QuizService) {
+    $scope.quiz = QuizService.getQuiz();
+
+    console.log($scope.quiz);
+
+    $scope.goToResult = (meaning) => {
+      let result = 0;
+      if (meaning === $scope.quiz.question.meaning)
+        result = 1;
+      $location.path(`/result/${result}`);
+    }
+  }
+]);
+
+module.controller('ResultController', [
+  '$scope',
+  '$location',
+  '$routeParams',
+  'QuizService',
+  'ImageService',
+  function($scope, $location, $routeParams, QuizService, ImageService) {
+
+    $scope.linkClick = () => {
+      $location.path(`/`);
+    }
+
+    $scope.result = $routeParams.result === '1';
+
+    if ($scope.result) {
+      // Use another module to get image
+      $scope.text = "Tiếp tục";
+      QuizService.increaseScore(1);
+      $scope.resultUrl = ImageService.getRewardImage(QuizService.score);
+    } else {
+      $scope.text = "Bắt đầu lại";
+      $scope.resultUrl = ImageService.getPunishImage();
+      QuizService.reset();
+    }
+  }
+]);
+
+module.config([
+  '$routeProvider',
+  '$locationProvider',
+  '$compileProvider',
+  function($routeProvider, $locationProvider, $compileProvider) {
+    // Remove :unsafe from chrome extension
+    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|local|data|chrome-extension):/);
+
+    $routeProvider.when('/', {
+      templateUrl: 'js/app/template/quiz.html',
+      controller: 'IndexController'
+    }).when('/result/:result', {
+      templateUrl: 'js/app/template/result.html',
+      controller: 'ResultController'
+    });
+  }
+]);
